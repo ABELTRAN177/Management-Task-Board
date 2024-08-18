d429a8d3938150edd9d89b62b4d78a5430425f74
Angel B <angelb@Angels-MacBook-Pro.local>
2024-08-18 16:57:34 -0700

    my first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+    const timestamp = new Date().getTime(); // Get current timestamp
+    const randomNum = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 999
+
+    return `${timestamp}-${randomNum}`;
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+
+}
+
+// Todo: create a function to handle adding a new task
+function handleAddTask(event){
+
+}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
+event.target.closest('.task').remove();
+} else {
+    console.log('Task not found');
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+
+});
