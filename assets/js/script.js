4625021a5aea3e261ba679d11219ba56996888f6
Angel B <angelb@Angels-MacBook-Pro.local>
2024-08-25 08:26:55 -0700

    created modal html, created due date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) ;
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
@@ -10,34 +10,40 @@ function generateTaskId() {
     return `${timestamp}-${randomNum}`;
 }
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
+// created a function where the due date color changes based on the number of days until the due date
+// if it falls within 5 days, the color will be red
+// if it falls within 20 days, the color will be orange
+// if it falls within 21 days or more, the color will be yellow
+function dueDateColorChange(dueDate) {
+    const currentDate = new Date();
+    const taskDueDate = new Date(dueDate);
+    const differenceTime = Math.abs(taskDueDate - currentDate);
+    const differenceDays = Math.ceil(differenceTime / (1000 * 3600 * 24));
 
+    if (differenceDays <= 5) {
+        return 'red';
+    } else if (differenceDays <= 20) {
+        return 'orange';
+    } else {
+        return 'yellow';
+    }
 }
 
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
 
+// Todo: create a function to render the task list and make cards draggable
+// here we are using the map method to create a new array with the results of calling a provided function on every element in the calling array
+function renderTaskList() {
+    const todoContainer = document.getElementById('todo');
+    todoContainer.innerHTML = taskList.map(createTaskCard).join('');
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
 
-}
+// Todo: create a function to create a task card
+
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-event.target.closest('.task').remove();
-} else {
-    console.log('Task not found');
-}
 
-// Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
 
-});
